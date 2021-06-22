07478e82b5bc41191da4c4976b5d2781336cc8dc
aSTRonunn <vitormanoel00710@gmail.com>
2021-06-22 13:02:11 -0300

    🚀update: arquivos js adicionados

---
+++ discover-javascript/03-Variáveis/nomeVariaveis.js
@@ -0,0 +1,19 @@
+/* 
+    JS case-sensitive (sensível ao caso)
+    JS aceita a cadeia de caracteres Unicode
+*/
+
+
+let Álisson = 'Álisson'
+
+let Alisson = 'Alisson'
+
+let alisson = 'alisson'
+
+let álisson = 'álisson'
+
+let $valor = 2
+
+let _io = true
+
+console.log('oi')
---
+++ discover-javascript/04-Praticando/declaration.js
@@ -0,0 +1,28 @@
+// Variáveis e tipos de dados
+
+var name
+
+// assignment or atribuition of values
+
+name = "Mike"
+
+// que tipo de dado foi colocado na variável?
+
+console.log(name)
+
+// agrupamento de declarations
+let age, isHuman
+
+age = 18
+isHuman = true
+
+// multiplos argumentos na função
+//console.log(name, age, isHuman)
+
+// escrita de texto + variáveis
+// concatenando valores
+console.log('O '+ name +' tem '+ age +' anos.')
+
+// interpolando valores com templete literals or template strings
+console.log(`O ${name} tem ${age} anos.`)
+
