86f9eec3a02647104f9b0d11a3bae56c60a08fbe
aSTRonunn <vitormanoel00710@gmail.com>
2021-07-06 14:41:08 -0300

    🚀 update: arquivos js adicionados

---
+++ discover-javascript/07-Expressões e Operadores/scripts/assigments-operators.js
@@ -0,0 +1,32 @@
+// Operadores de atribuição (Assignment)
+let x
+
+console.log(x)
+
+// assigment
+x = 1
+console.log(x)
+
+// addition assigment
+x += 2
+console.log(x)
+
+// subtraction assigment
+x -= 1
+console.log(x)
+
+// multiplication assigment
+x *= 2
+console.log(x)
+
+// division assigment
+x /= 2
+console.log(x)
+
+// exponentiation assigment
+x **= 4
+console.log(x)
+
+// remainder  assigment
+x %= 4
+console.log(x)
