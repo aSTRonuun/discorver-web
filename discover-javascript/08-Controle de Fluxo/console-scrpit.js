a85348aab8cff1b23886d00d010a396b3ef1990e
aSTRonunn <vitormanoel00710@gmail.com>
2021-07-12 12:52:34 -0300

    🚀 arquivos js control flow adicionados

---
+++ discover-javascript/08-Controle de Fluxo/console-scrpit.js
@@ -0,0 +1,14 @@
+// if... else
+
+let temperature = 37.5
+let hightTemperature = temperature >= 37.5
+let mediumTemperature = temperature >= 37
+
+
+if(hightTemperature){
+    console.log("Está com febre alta.")
+} else if(mediumTemperature){
+    console.log("Está com febre moderada")
+}else{
+    console.log("Saudável.")
+}
