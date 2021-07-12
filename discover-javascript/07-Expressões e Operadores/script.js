f82cc85244a0612666db93aa1179849d16d962d5
aSTRonunn <vitormanoel00710@gmail.com>
2021-07-12 12:37:43 -0300

    🚀 update: arquivos js operators and expressions adicionados

--- discover-javascript/07-Expressões e Operadores/script.js
+++ discover-javascript/07-Expressões e Operadores/script.js
@@ -1,4 +1,17 @@
+/*
+    Operator precendence
+        Precedência de operadores
 
+    1 - grounpin                    ( )
+    2 - negacao e incremento        ! ++ --
+    3 - multiplição e divisão       * /
+    4 - adição e subtração          + -
+    5 - relacional                  < <= > >=
+    6 - igualdade                   == != === !==
+    7 - AND                         &&
+    8 - OR                          ||
+    9 - condicional                 ? :
+    10 - assigment (atribuição)     = += -= *= /= %=
+*/
 
-
-
+console.log(3 > 2 && 2 > 1)
---
+++ discover-javascript/07-Expressões e Operadores/scripts/string-operator.js
@@ -0,0 +1,13 @@
+// Operador de String (String operator)
+
+// comparison (comparação)
+// console.log('A' == 'a')
+
+// concatenation (concatenação)
+// Retorna a união de duas String
+console.log('a' + 'A')
+
+let string = 'alpha'
+string += 'bet'
+console.log(string + 7000)
+
